33559dbf2d82785b496c2f936c49036eada04146
Tiang Ann Linus Tay <s10195119@connect.np.edu.sg>
2022-01-05 07:31:31 +0800

    Test

--- js/report.js
+++ js/report.js
@@ -1,5 +1,3 @@
-GitHub_Information = require('../text.txt')
-
 function getData() {
     var successRate = document.getElementById('successRate').getElementsByClassName('percent')[0].innerHTML;
     console.log('test')
@@ -7,25 +5,7 @@ function getData() {
     {
         console.log("The success rate of all tests is: " + successRate)
     }
-    var x = readFile(GitHub_Information)
-    console.log(x)
+
 }
 
 window.onload = getData;
-
-function readFile(input) {
-    let file = input.files[0];
-  
-    let reader = new FileReader();
-  
-    reader.readAsText(file);
-  
-    reader.onload = function() {
-      console.log(reader.result);
-    };
-  
-    reader.onerror = function() {
-      console.log(reader.error);
-    };
-  
-  }
